2c6e14ba7c31926dd4ed6b75e6f2036b705639d4
ZamurVedovatto <vedovatto@gmail.com>
2018-03-11 22:50:19 -0300

    protected requests and auth guard

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -15,14 +15,16 @@ import { RegisterComponent } from './components/register/register.component';
 import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'register', component: RegisterComponent },
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'profile', component: ProfileComponent}
+  {path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate:[AuthGuard]},
+  {path:'**', redirectTo: ''}
 ];
 
 @NgModule({
@@ -42,7 +44,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule.forRoot()
   ],
-  providers: [ValidateService, AuthService],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
